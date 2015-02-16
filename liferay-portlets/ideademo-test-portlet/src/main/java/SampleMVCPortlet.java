05ad10edcd56759460951464a0fc5689f415904a
Krzysztof Gołębiowski <krzysiek.golebiowski@gmail.com>
2015-02-16 08:50:43 +0100

    Added some sample code.

--- liferay-portlets/ideademo-test-portlet/src/main/java/SampleMVCPortlet.java
+++ liferay-portlets/ideademo-test-portlet/src/main/java/SampleMVCPortlet.java
@@ -1,3 +1,7 @@
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.model.User;
+import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.util.bridges.mvc.MVCPortlet;
 
 import javax.portlet.PortletException;
@@ -10,8 +14,28 @@ import java.io.IOException;
  */
 public class SampleMVCPortlet extends MVCPortlet {
 
+    int firstVar;
+    int secondVar;
+    int thirdVar;
+
     @Override
-    public void render(RenderRequest request, RenderResponse response) throws PortletException, IOException {
+    public void render(RenderRequest request, RenderResponse response)
+            throws PortletException, IOException {
+
+        ThemeDisplay themeDisplay = (ThemeDisplay) request.getAttribute(WebKeys.THEME_DISPLAY);
+        User loggedUser = themeDisplay.getUser();
+
+        Group scopeGroup = themeDisplay.getScopeGroup();
+
+        getClass().getResourceAsStream("/rebel.xml");
+
+        firstVar = 1;
+        secondVar = 2;
+
+        thirdVar = firstVar + secondVar;
+
+        System.out.println(loggedUser.getFullName());
+
         super.render(request, response);
     }
 }
